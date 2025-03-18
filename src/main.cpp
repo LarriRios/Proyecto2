f36e2ee941b31bec08674c67d37046bc6d1cc805
larry rios sanchez <larririos@unicauca.edu.co>
2025-03-18 15:58:37 -0500

    Agregué funciones y variables booleanas

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,18 @@
 # define SW3_pin 17  // Tercer switch
 # define SW4_pin 18  // Cuarto switch
 
-bool ledStates[5]={false, false, false, false, false}; //Vector para estados de LEDs
-bool switchSate= false; //
+bool estadoLEDs[5]={false, false, false, false, false}; //Vector para estados de LEDs
+bool ledsEncendidosPorSW1= false; 
+bool apagadoProgresivoHecho = false;
+
+void encenderProgresivoTodos();
+void apagarProgresivoTodos();
+void actualizarEstadoLEDs(bool estados[]);
+void setEstados(bool e0, bool e1, bool e2, bool e3, bool e4);
+
 
 void setup() {
+  Serial.begin(9600);
   // LEDs como salida
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
@@ -26,85 +34,140 @@ void setup() {
   pinMode(SW2_pin, INPUT);
   pinMode(SW3_pin, INPUT);
   pinMode(SW4_pin, INPUT);
+
+  setEstados(false, false, false, false, false);
+  actualizarEstadoLEDs(estadoLEDs);
 }
 
+
 void loop() {
-  int sw1= digitalRead(SW1_pin);
-  int sw2= digitalRead(SW2_pin);
-  int sw3= digitalRead(SW3_pin);
-  int sw4= digitalRead(SW4_pin);
+  bool sw1= digitalRead(SW1_pin);
+  bool sw2= digitalRead(SW2_pin);
+  bool sw3= digitalRead(SW3_pin);
+  bool sw4= digitalRead(SW4_pin);
 
-  //Switch 1 activado: Encendido progresivo de TODOS los LEDs
-  if(sw1==HIGH && !switchSate){
-    switchSate=true;
-    for(int luz = 0; luz <= 255; luz++){
-      analogWrite(LED1,luz);
-      analogWrite(LED2,luz);
-      analogWrite(LED3,luz);
-      analogWrite(LED4,luz);
-      analogWrite(LED5,luz);
-      delay(10);
-    }
-
-    for(int i = 0; i<5;i++) ledStates[i]=true;
-  }
-
-  if(sw1==LOW && switchSate){
-    switchSate=false;
-    for(int luz = 255; luz >= 0; luz--){
-      analogWrite(LED1,luz);
-      analogWrite(LED2,luz);
-      analogWrite(LED3,luz);
-      analogWrite(LED4,luz);
-      analogWrite(LED5,luz);
-      delay(10);
-    }
-
-    for(int i = 0; i<5;i++) ledStates[i]=true;
-  }
+  bool sw5= sw2 && sw3 && sw4;
+  bool sw6= sw3 && sw4;
+  bool sw7= sw2 && sw3;
+  bool sw8= sw2 && sw4;
+    
 
   int opcion= 0;
-  if(sw2==HIGH)opcion=1;
-  else if(sw3==HIGH)opcion=2; 
-  else if(sw4==HIGH)opcion=3;
+  if(sw1)opcion=1;
+  else if(sw5)opcion=5; 
+  else if(sw6)opcion=6;
+  else if(sw5)opcion=7; 
+  else if(sw6)opcion=8;
+  else if(sw4 && !sw1 && !sw2 && !sw3)opcion=4;
+  else if(sw3 && !sw1 && !sw2 && !sw4)opcion=3; 
+  else if(sw2 && !sw1 && !sw3 && !sw4)opcion=2;
+  else opcion=0;
   
+  if(!sw1 && ledsEncendidosPorSW1 && !apagadoProgresivoHecho){
+    apagarProgresivoTodos();
+    ledsEncendidosPorSW1=false;
+    apagadoProgresivoHecho = true;
+    delay(500);
+    return;
+  }
+
+  if(sw1) apagadoProgresivoHecho = false;
+
+
   switch (opcion)
   {
   case 1:
-    analogWrite(LED1,255);
-    ledStates[0]=true;
+    Serial.println("Switch 1 activado: Encendido progresivo de TODOS los LEDs");
+    encenderProgresivoTodos();
+    ledsEncendidosPorSW1 = true;
     break;
 
   case 2:
-    analogWrite(LED2,255);
-    analogWrite(LED3,255);
-    ledStates[1]=true;
-    ledStates[2]=true;
+    Serial.println("Switch 2 activado: Encender LED1");
+    setEstados(true, false, false, false, false);
+    actualizarEstadoLEDs(estadoLEDs);
     break;
+
   case 3:
-    analogWrite(LED4,255);
-    analogWrite(LED5,255);
-    ledStates[3]=true;
-    ledStates[4]=true;
-    break;
+    Serial.println("Switch 3 activado: Encender LED2 y LED3");
+    setEstados(false, true, true, false, false);
+    actualizarEstadoLEDs(estadoLEDs);
+  break;
+
+  case 4:
+    Serial.println("Switch 4 activado: Encender LED4 y LED5");
+    setEstados(false, false, false, true, true);
+    actualizarEstadoLEDs(estadoLEDs);
+  break;
+    
   default:
-    if(sw2==LOW){
-      analogWrite(LED1,0);
-      ledStates[0]=false;
-    }
-    if(sw3==LOW){
-      analogWrite(LED2,0);
-      analogWrite(LED3,0);
-      ledStates[1]=false;
-      ledStates[2]=false;
-    }
-    if(sw4==LOW){
-      analogWrite(LED4,0);
-      analogWrite(LED5,0);
-      ledStates[3]=false;
-      ledStates[4]=false;
-    }
-    break;
+    setEstados(false, false, false, false, false);
+    actualizarEstadoLEDs(estadoLEDs);
+  break;
   }
-  delay(2000);
+  delay(500);
+}
+
+void setEstados(bool e0, bool e1, bool e2, bool e3, bool e4) {
+  estadoLEDs[0] = e0;
+  estadoLEDs[1] = e1;
+  estadoLEDs[2] = e2;
+  estadoLEDs[3] = e3;
+  estadoLEDs[4] = e4;
+}
+
+// Actualiza físicamente los LEDs según el array estadoLEDs
+void actualizarEstadoLEDs(bool estados[]) {
+  int pines[5] = {LED1, LED2, LED3, LED4, LED5};
+
+  for (int i = 0; i < 5; i++) {
+    if (estados[i]) {
+      analogWrite(pines[i], 255);  // Encendido total
+    } else {
+      analogWrite(pines[i], 0);    // Apagado
+    }
+  }
+}
+
+// ==============================
+// Encendido progresivo
+void encenderProgresivoTodos() {
+  int pines[5] = {LED1, LED2, LED3, LED4, LED5};
+
+  if (estadoLEDs[0] && estadoLEDs[1] && estadoLEDs[2] && estadoLEDs[3] && estadoLEDs[4]) {
+    Serial.println("Todos los LEDs ya están encendidos");
+    return;
+  }
+
+  int pwm = 0;
+  while (pwm <= 255) {
+    for (int i = 0; i < 5; i++) {
+      analogWrite(pines[i], pwm);
+    }
+    pwm += 5;
+    delay(30);
+  }
+
+  setEstados(true, true, true, true, true);
+}
+
+// Apagado progresivo
+void apagarProgresivoTodos() {
+  int pines[5] = {LED1, LED2, LED3, LED4, LED5};
+
+  if (!estadoLEDs[0] && !estadoLEDs[1] && !estadoLEDs[2] && !estadoLEDs[3] && !estadoLEDs[4]) {
+    Serial.println("Todos los LEDs ya están apagados");
+    return;
+  }
+
+  int pwm = 255;
+  while (pwm >= 0) {
+    for (int i = 0; i < 5; i++) {
+      analogWrite(pines[i], pwm);
+    }
+    pwm -= 5;
+    delay(10);
+  }
+
+  setEstados(false, false, false, false, false);
 }
